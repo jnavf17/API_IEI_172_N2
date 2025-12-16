15481fc84be63f5c062e1b15d502f9c0690f6dbf
jnavf17 <jignacionavarretef14@gmail.com>
2025-12-16 13:04:56 -0300

    jnavf17

--- inicio.py
+++ inicio.py
@@ -1,5 +1,5 @@
 from negocio import obtener_data_users_api, listado_users_db,crear_user_api, modificar_user_api, eliminar_user_api
-from negocio import crear_post_api,modificar_post_api,eliminar_post_api 
+from negocio.negocio_posts import crear_post_api,modificar_post_api,eliminar_post_api 
 
 #obtener_data_users_api('https://jsonplaceholder.typicode.com/users')
 listado_users_db()
